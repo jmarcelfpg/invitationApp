175d12c301531803475131a615e1db6f63183657
jmarcelfpg <jmarcel_fpg@hotmail.com>
2018-05-02 16:51:01 -0700

    Sending info to the server

--- src/public/js/index.ts
+++ src/public/js/index.ts
@@ -1,1 +1,20 @@
-console.log('working !');
+import axios from 'axios';
+
+const loginForm = (document.getElementById('login') as HTMLFormElement);
+
+if (loginForm) {
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const EMAIL = 'email';
+        const PASSWORD = 'password';
+        const data = {
+            email: (loginForm.elements.namedItem(PASSWORD) as HTMLInputElement).value,
+            password: (loginForm.elements.namedItem(EMAIL) as HTMLInputElement).value,
+        };
+        axios.post('/login', data).then((response) => {
+            if (response.statusText === 'OK') {
+                window.location.assign('/board');
+            }
+        });
+    });
+}
