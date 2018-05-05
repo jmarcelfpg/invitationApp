97c47a76264ec138ed1394782a3a514a982f427a
jmarcelfpg <jmarcel_fpg@hotmail.com>
2018-05-05 13:45:41 -0700

    new routes and db almost ready

---
+++ src/developmentServer/routes/index.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+import path from 'path';
+import * as user from './user';
+
+export const User = user;
+
+export const board: RequestHandler = (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'board.html'));
+};
+export const admin: RequestHandler = (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+};
+export const profile: RequestHandler = (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'profile.html'));
+};
+export const resetPass: RequestHandler = (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'resetPass.html'));
+};
+export const registered: RequestHandler = (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'registered.html'));
+};
+export const register: RequestHandler = (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+};
+export const registerAdmin: RequestHandler = (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'registerAdmin.html'));
+};
