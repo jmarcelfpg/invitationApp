7939966e5e0a9e3d75fa3c49f27c9da2a0350422
jmarcelfpg <jmarcel_fpg@hotmail.com>
2018-05-05 19:14:11 -0700

    fixing changes

--- config/express.js
+++ config/express.js
@@ -19,44 +19,63 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const bodyParser = __importStar(require("body-parser"));
-const morgan_1 = __importDefault(require("morgan"));
-const http_1 = __importDefault(require("http"));
-const path_1 = __importDefault(require("path"));
 const compression_1 = __importDefault(require("compression"));
-const express_session_1 = __importDefault(require("express-session"));
 const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
+const morgan_1 = __importDefault(require("morgan"));
 const passport_1 = __importDefault(require("passport"));
+const connect_flash_1 = __importDefault(require("connect-flash"));
+const path_1 = __importDefault(require("path"));
 function default_1() {
     return __awaiter(this, void 0, void 0, function* () {
-        const app = express_1.default();
-        if (process.env.NODE_ENV === 'development') {
-            app.use(morgan_1.default('dev'));
+        try {
+            const app = express_1.default();
+            if (process.env.NODE_ENV === 'development') {
+                app.use(morgan_1.default('dev'));
+            }
+            else if (process.env.NODE_ENV === 'production') {
+                app.use(compression_1.default());
+            }
+            app.use(bodyParser.urlencoded({ extended: true }));
+            app.use(bodyParser.json());
+            app.use(express_session_1.default({
+                resave: true,
+                saveUninitialized: true,
+                secret: 'config.sessionSecret',
+            }));
+            app.use(connect_flash_1.default());
+            app.use(passport_1.default.initialize());
+            app.use(passport_1.default.session());
+            app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'public')));
+            app.set('port', (process.env.PORT || 3000));
+            app.get('/board', (req, res, next) => {
+                res.sendFile(path_1.default.join(__dirname, '..', 'public', 'board.html'));
+            });
+            app.get('/resetPass', (req, res, next) => {
+                res.sendFile(path_1.default.join(__dirname, '..', 'public', 'resetPass.html'));
+            });
+            app.get('/register', (req, res, next) => {
+                res.sendFile(path_1.default.join(__dirname, '..', 'public', 'register.html'));
+            });
+            app.post('/register', (req, res, next) => {
+                const { email, password, name, lastName, contact, confirmation } = req.body;
+                console.log(JSON.stringify(req.body));
+                res.status(200).send('Hi');
+            });
+            app.post('/login', (req, res, next) => {
+                const { email, password } = req.body;
+                console.log(email, password);
+                res.send('Hi').status(200);
+            });
+            app.use('*', (req, res, next) => {
+                res.sendFile(path_1.default.join(__dirname, '..', 'public', 'index.html'));
+            });
+            return app;
         }
-        else if (process.env.NODE_ENV === 'production') {
-            app.use(compression_1.default());
+        catch (_a) {
+            throw new Error("error in the Express configuration");
         }
-        app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(bodyParser.json());
-        app.use(express_session_1.default({
-            saveUninitialized: true,
-            resave: true,
-            secret: "config.sessionSecret"
-        }));
-        app.use(passport_1.default.initialize());
-        app.use(passport_1.default.session());
-        app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
-        app.set('port', (process.env.PORT || 3000));
-        const server = http_1.default.createServer(app);
-        app.use('*', (req, res, next) => {
-            res.sendFile(path_1.default.join(__dirname, 'public', 'index.html'));
-        });
-        console.log('Starting Express Server');
-        server.listen(app.get('port'), () => {
-            console.log('Express listening on port ' + app.get('port'));
-        });
-        return server;
     });
 }
 exports.default = default_1;
-;
 //# sourceMappingURL=express.js.map
--- config/passport.js
+++ config/passport.js
@@ -1,36 +1,25 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const passport_1 = __importDefault(require("passport"));
-const user_1 = __importDefault(require("../models/user"));
+const user_1 = __importDefault(require("../app/models/user"));
 const local_1 = __importDefault(require("./strategies/local"));
 function default_1() {
-    return __awaiter(this, void 0, void 0, function* () {
-        passport_1.default.serializeUser(function (user, done) {
-            done(null, user.id);
-        });
-        passport_1.default.deserializeUser(function (id, done) {
-            user_1.default.findOne({
-                _id: id
-            }, '-password -salt', function (err, user) {
-                if (user) {
-                    done(err, user);
-                }
-            });
-        });
-        local_1.default();
-        return true;
+    passport_1.default.serializeUser((user, done) => {
+        done(null, user.id);
     });
+    passport_1.default.deserializeUser((id, done) => {
+        user_1.default.findOne({
+            _id: id,
+        }, '-password -salt', (err, user) => {
+            if (user) {
+                done(err, user);
+            }
+        });
+    });
+    local_1.default();
 }
 exports.default = default_1;
 //# sourceMappingURL=passport.js.map
